e316964ecefbb2b033fe5a1254ab89f4d82821c0
Dexter245 <Dexter245@googlemail.com>
2019-08-15 17:02:50 +0200

    added test code for font

--- include/View1.h
+++ include/View1.h
@@ -4,12 +4,14 @@
 #include "View.h"
 #include "Model1.h"
 #include "Image.h"
+#include "Font.h"
 
 class View1 : public dex::View {
 
 private:
     Model1 &model;
     dex::Image background;
+    dex::Font font;
 
 public:
     explicit View1(Model1 &model);
--- source/View1.cpp
+++ source/View1.cpp
@@ -4,7 +4,8 @@
 
 View1::View1(Model1 &model) :
     model(model),
-    background("background.png")
+    background("background.png"),
+    font("DroidSansMono.ttf", 18)
 {
     std::cout << "View1 constructor" << std::endl;
     std::cout << "background: " << &background << std::endl;
@@ -13,4 +14,6 @@ View1::View1(Model1 &model) :
 void View1::render() {
 //    std::cout << "view1 render" << std::endl;
     background.draw(0, 0);
+    font.renderText("fontTest", 50, 50);
+    font.renderText("fontTest", 50, 150, {255, 0, 0, 255});
 }
